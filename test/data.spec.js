eda42330d6c289d46c389333ba889c8094d71e58
fabiola29298 <fabiola29298@gmail.com>
2021-05-07 16:54:42 -0400

    agregando describes para home y wiki

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,5 +1,34 @@
 import { example, anotherExample } from '../src/data.js';
 
+describe('Home', () => {
+    // should get 8 pokemon: name, description and id. For suggestions
+    it('should return', () => {
+      // expect(example.getPokemons(8)).toBe('function');
+    });
+
+});
+
+describe('Wiki', () => {
+  describe('/GET Pokemon'), () => {
+    it('should return', () => {
+      // expect(example.getPokemons(8)).toBe('function');
+    });
+  }
+  describe('/GET Stats'), () => {
+    it('should return', () => {
+      // expect(example.getPokemons(8)).toBe('function');
+    });
+  }
+});
+//
+// HOME ****************
+// should get 8 pokemon: name, description and id. For suggestions
+// WIKI ****************
+// should get the image of the pokemon
+// should get the pokemon's statistics
+// should get images of the evolutions
+// GAME ****************
+// should get the attack of two pokemon
 
 describe('example', () => {
   it('is a function', () => {

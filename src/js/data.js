e05169720abc2e0900b60dbb6c5137d6ba850521
fabiola29298 <fabiola29298@gmail.com>
2021-05-12 16:52:23 -0400

    added functions and class descriptions

--- src/js/data.js
+++ src/js/data.js
@@ -1,9 +1,45 @@
-// estas funciones son de ejemplo
 
-export const example = () => {
-  return 'example';
-};
-
-export const anotherExample = () => {
-  return 'OMG';
-};
+/**
+   * Get a pokemon by id
+   * @return {pokemon} A pokemon object.
+   */
+export const getPokemon = (id) => {
+  // ...
+}
+/**
+ * Get X amount of pokemons
+ * @return {array[pokemon]} A list of pokemon objects.
+ */
+export const getPokemons = (str) => {
+  // ...
+}
+/**
+ * Get pokemons by type
+ * @return {array[pokemon]} A list of pokemon objects of the same type.
+ */
+export const getPokemonsbyType = (type) => {
+  // ...
+}
+/**
+ * Get two random pokemon
+ * @return {array[pokemon, pokemon]} Two pokemon objects.
+ */
+export const getRandomPokemons = () => {
+  // ...
+}
+  /**
+   * Convert the weight of hectograms to kilograms.
+   * @param {number} - The weight in hectograms.
+   * @return {number} - The weight in kilograms.
+   */
+export const fromHectograms = (str) => {
+  // ...
+}
+  /**
+   * Convert the height of decimetres to centimeters.
+   * @param {number} - The height in decimetres.
+   * @return {number} - The height in centimeters.
+   */
+export const fromDecimetres = (str) => {
+  // ...
+}
--- src/pokemon.js
+++ src/pokemon.js
@@ -1,21 +1,24 @@
-class Pokemon {
-  constructor(
-    id,
-    name,
-    height,
-    weight,
-    abilities,
-    image,
-    type,
-    forms
-  ) {
+  /** Class representing a pokemon. */
+  class Pokemon {
+    /**
+     * Create a pokemon.
+     * @param {number} id - The identifier.
+     * @param {string} name - The name.
+     * @param {number} height - The height of this Pokémon in centimeters.
+     * @param {number} weight - The weight of this Pokémon in kilograms.
+     * @param {array[ability]} abilities - A list of abilities this Pokémon could potentially have.
+     * @param {string} image - The url of the pokemon image.
+     * @param {array[type]} types - A list of details showing types this Pokémon has.
+     * @param {array[form]} forms - A list of forms this Pokémon can take on.
+     */
+  constructor( id, name, height, weight, abilities, image, types, forms) {
     this.id = id;
     this.name = name;
     this.height = height;
     this.weight = weight;
     this.abilities = abilities;
     this.image = image;
-    this.type = type;
+    this.types = types;
     this.forms = forms;
   }
 }

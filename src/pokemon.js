3bbade6a61e56bc2dcf69bf6b1e32eeeb7ed59e5
rodrigojgq <rodrigojgq@gmail.com>
2021-05-11 21:52:49 -0400

    Create stats and pokemon classes

---
+++ src/pokemon.js
@@ -0,0 +1,27 @@
+class Pokemon {
+  constructor(
+    id,
+    name,
+    height,
+    weight,
+    abilities,
+    moves,
+    image,
+    stats,
+    type,
+    forms,
+    evolutions
+  ) {
+    this.id = id;
+    this.name = name;
+    this.height = height;
+    this.weight = weight;
+    this.abilities = abilities;
+    this.moves = moves;
+    this.image = image;
+    this.stats = stats;
+    this.type = type;
+    this.forms = forms;
+    this.evolutions = evolutions
+  }
+}

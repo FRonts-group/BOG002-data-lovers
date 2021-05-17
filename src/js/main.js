426a12a6804f196acdf68c1699b350e3a2ab5ffd
fabiola29298 <fabiola29298@gmail.com>
2021-05-17 15:47:11 -0400

    showed the pokemon on the carousel

--- src/js/main.js
+++ src/js/main.js
@@ -14,3 +14,25 @@ console.log(lots);
 // let pokemon = new Pokemon(1,'',1,1,1,'',1,1);
 // let stats = new Stats(1,1,1,1,1,1,1);
 // console.log(pokemon,stats);
+
+
+// show the carrusel in index
+let amountSuggestions = 8;
+let suggestions = await getPokemons(amountSuggestions);
+let containerCarrusel = document.querySelector('#suggested-pokemon');
+  for (let item of suggestions){
+
+    containerCarrusel.innerHTML += `
+    <div class="pokemon-card">
+    <img src=${item.image} alt="Pokemon">
+    <div class="pokemon-info--container">
+      <h3>${item.name}</h3>
+      <p>Type: ${item.types[0]} <br>
+      Height: ${item.height} cm <br>
+      Weight: ${item.weight} kg </p>
+      <button>More</button>
+    </div>
+    </div>`;
+  }
+
+

798feeb41ec99da8b2663699141c4f6f28e68445
rodrigojgq <rodrigojgq@gmail.com>
2021-05-19 19:04:28 -0400

    Add interaction to some buttons

--- src/js/main.js
+++ src/js/main.js
@@ -2,36 +2,40 @@
 // import Stats from './stats.js';
 import { getPokemon, getPokemons} from './data.js';
 
+let containerCarousel = document.querySelector('#suggested-pokemon');
+let gameButton = document.getElementById("game-button");
+let status = document.querySelector(".statusSearch");
+
 
 // show the carousel in index
-let amountSuggestions = 8;
-let suggestions = await getPokemons(120, amountSuggestions);
-let containerCarousel = document.querySelector('#suggested-pokemon');
-let status = document.querySelector(".statusSearch");
-// show the content of the cards
-for (let item of suggestions){
-
-  containerCarousel.innerHTML += `
-  <div class="pokemon-card">
-  <img src=${item.image} alt="Pokemon">
-  <div class="pokemon-info--container">
-  <h3>${item.name}</h3>
-  <p>Type: ${item.types[0]} <br>
-  Height: ${item.height} cm <br>
-  Weight: ${item.weight} kg </p>
-  <button value="${item.id}"  class="button-card">More</button>
-  </div>
-  </div>`;
-}
-// add the function to display pokemon in Wiki
-document.querySelectorAll('.button-card').forEach(card =>
-  card.addEventListener("click", () => {
-    localStorage.setItem("pokemon", card.value);
-    location.href = './wiki.html';
+const showCarousel = async () => {
+  let amountSuggestions = 8;
+  let suggestions = await getPokemons(120, amountSuggestions);
+  for (let item of suggestions){
+    containerCarousel.innerHTML += `
+    <div class="pokemon-card">
+    <img src=${item.image} alt="Pokemon">
+    <div class="pokemon-info--container">
+    <h3>${item.name}</h3>
+    <p>Type: ${item.types[0]} <br>
+    Height: ${item.height} cm <br>
+    Weight: ${item.weight} kg </p>
+    <button value="${item.id}"  class="button-card">More</button>
+    </div>
+    </div>`;
+  }
+  document.querySelectorAll('.button-card').forEach(card =>
+    card.addEventListener("click", () => {
+      localStorage.setItem("pokemon", card.value);
+      location.href = './wiki.html';
   }));
+}
+
+showCarousel();
+// show the content of the cards
+// add the function to display pokemon in Wiki
 
 //search a  pokemon
-
 document.addEventListener("keypress", async e => {
   if (e.target.matches("#search")) {
     if (e.key === "Enter") {
@@ -47,7 +51,14 @@ document.addEventListener("keypress", async e => {
           location.href = './wiki.html';
         }
       }catch (err) {
+        status.style.color = "red";
+        status.innerHTML = `<p>No results were found. Please try again.</p>`;
       }
     }
   }
 });
+
+// Add the function to go to game page
+gameButton.addEventListener('click', () => {
+  location.href = './game.html';
+})

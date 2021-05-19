fc5884cc00101471bd2973d8f5dfa6e3d1aa5dda
fabiola29298 <fabiola29298@gmail.com>
2021-05-19 18:08:45 -0400

    fixed scroll in game

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,6 @@
-import Pokemon from './pokemon.js';
-import Stats from './stats.js';
-import { getPokemon, getPokemons, getPokemonStats} from './data.js';
-/*
-// New pokemon id 12
-console.log( await getPokemon(12));
-// New pokemon-stats id 12
-console.log(await getPokemonStats(12));
-// 4 pokemons for the carrusel
-let lots = await getPokemons(4,8);
-console.log(lots);
-*/
-// Probando pull request
-// let pokemon = new Pokemon(1,'',1,1,1,'',1,1);
-// let stats = new Stats(1,1,1,1,1,1,1);
-// console.log(pokemon,stats);
+// import Pokemon from './pokemon.js';
+// import Stats from './stats.js';
+import { getPokemon, getPokemons} from './data.js';
 
 
 // show the carousel in index
